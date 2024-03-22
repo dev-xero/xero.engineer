ff8dad691402fa559812f9af536e3d2a30e43cd1
dev-xero <elo48858@gmail.com>
2024-03-23 00:12:38 +0100

    conf: Add typescript checks

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  typescript: {
+    strict: true,
+    typeCheck: true,
+  },
+  modules: [
+    "@nuxtjs/eslint-module"
+  ]
 })
