5db161c59db3edf7860fa8837b188048b22b8faa
dev-xero <elo48858@gmail.com>
2024-03-24 00:34:33 +0100

    feat: Add introduction section

---
+++ components/Introduction.vue
@@ -0,0 +1,12 @@
+<template>
+    <section class="introduction contained">
+        <p class="paragraph jp">こんにちは、私のウェブサイトへようこそ</p>
+        <h1 class="figure-head">HEY, I'M XERO!</h1>
+        <p class="paragraph bio">
+            Hi, nice to meet you, I'm a 17 year old Computer Science student,
+            interested in programming, art, anime, and chess. I usually focus on
+            backend web and android development, with some low-level rust and c
+            stuff occasionally.
+        </p>
+    </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
     <NavigationBar />
+    <Introduction />
 </template>
 
 <script setup lang="ts">
