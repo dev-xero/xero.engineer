9eb643f21a006ae69a2b480d26a3fb3ef9a356ca
dev-xero <elo48858@gmail.com>
2024-03-23 01:09:52 +0100

    feat: Create index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,13 @@
 <template>
-    <h1>Xero Engineer</h1>
+   <!DOCTYPE html>
+   <html lang="en">
+   <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Xero // Home</title>
+   </head>
+   <body>
+        <h1>Xero: Engineer</h1>
+   </body>
+   </html>
 </template>
